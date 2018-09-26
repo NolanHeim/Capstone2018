9bbd043858ba721cbc3912b600b8e917dde9a051
Jordan Jones <jordanjones22t@gmail.com>
2018-09-26 16:09:31 -0700

    Initial commit for first classes

---
+++ IOG/Mission Creator.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# CLASSNAME.py
+#
+# DESCRIPTION
+#
+# Initial Creation Date: 09/26/2018
+#
+# Written by Jordan Jones and Nolan Heim
+#
+
+class ClassName:
+    
+    #TODO insert class functions and parameters
+
