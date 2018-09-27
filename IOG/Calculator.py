b554711eda9300c74919b784a5f461f3e1eae86d
Nolan Heim <n242@heim.ca>
2018-09-26 21:36:56 -0700

    Started Initial Framework

--- IOG/Calculator.py
+++ IOG/Calculator.py
@@ -8,7 +8,24 @@
 #
 # Written by Jordan Jones and Nolan Heim
 #
+#For numpy.piecewise
+import numpy
 
-class ClassName:
+class Calculator:
     
-    #TODO insert class functions and parameters
+    def __init__(self):
+        self.constant = 5
+    
+    #Returns the cubic Hermite polynomial function on the
+    #subinterval [subt1,subt2]
+    def cubicHermitePolynomial(self, subt1, subt2, func):
+        condition = lambda t: subt1 <= t <= subt2        
+        h = subt2-subt1
+        f1 = func[subt1]
+        f2 = func[subt2]
+        Ci = lambda t: h*f1*f2*t#Function Definition Here
+        
+        return [condition, Ci]
+        
+    def visibilityFunction(self, t1, t2, func, N):
+        print('Xi')
