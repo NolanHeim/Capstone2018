8b8f151bd73244bdec2d3d52ee45cf3c23fd2b3b
Jordan Jones <jordanjones22t@gmail.com>
2018-10-01 13:52:47 -0700

    removed data parsing from toolchain. parser is now run independently for satellite data, and mission creator loads data

--- IOG/Mission Creator.py
+++ IOG/Mission Creator.py
@@ -13,25 +13,44 @@
 from Parser import *
 from Constellation import *
 from Calculator import *
-
+import numpy as np
 
 class MissionCreator:
 
 
     #initialization function for the mission creator class    
-    def __init__(self, datapath, missionpath):
+    def __init__(self, datapath, missionpath, parsed_datapath):
 
         self.datapath = datapath
         self.missionpath = missionpath
+        self.parsed_datapath = parsed_datapath
         #initialize dependent objects
         self.system_setup(datapath, missionpath) 
         
         #parser already contains missionpath
         missions = self.parser.create_missions()
-        dataMatrices = self.parser.make_data_matrices()       
+        dataMatricesWithExtraInfo = self.load_data_matrices()
+
+        dataMatrices = []
+        print("L = "+str(len(dataMatricesWithExtraInfo)))
+        for matrix in range(0, len(dataMatricesWithExtraInfo)):
+            dataMatrix = []
+            for i in range(0, len(dataMatricesWithExtraInfo[matrix][1])):
+                dataLine = []
+                for j in range(0, len(dataMatricesWithExtraInfo[matrix][1][i])):
+                    dataLine.append(float(dataMatricesWithExtraInfo[matrix][1][i][j]))
+                dataMatrix.append(dataLine)            
+            dataMatrices.append(dataMatrix)
+
+        #Data Loading Test Code
+        print(len(dataMatrices))
+        print(len(dataMatrices[0]))
+        print(len(dataMatrices[0][0]))
         
         for mission in missions:        
-            self.generate_imaging_opportunities(mission, dataMatrices) #main functions
+           self.generate_imaging_opportunities(mission, dataMatrices) #main functions
+        
+        #Mission test code        
         print(str(len(missions)))
         print(missions[0].get_name())
         print(missions[0].get_coordinates())
@@ -46,11 +65,24 @@ class MissionCreator:
     
     def generate_imaging_opportunities(self, mission, dataMatrices):
         self.calculator.generate_imaging_opportunities(mission, dataMatrices)
-        print('hi')
 
+        
+    def load_data_matrices(self):
+        dataMatrices = []
+        #self.parser.parse_data(self.parsed_datapath)
+        #print("MC PDP is "+self.parsed_datapath)
+        for filename in os.listdir(self.parsed_datapath):
+            #dataMatrices.append(self.load(self.parsed_datapath+filename))
+            dataMatrices.append(np.load(self.parsed_datapath+filename))
+            #dataMatrices = (np.load(self.parsed_datapath+filename))
+                
+        
+        return dataMatrices
+    
+    
 #main code goes here
 #we will eventually define a main function so that the program can run as an executable
 
 #The two parameters are the filepaths for the satellite data and the mission info data, respectively
-missionCreator = MissionCreator("../../Data", "../../Missions/")
+missionCreator = MissionCreator("../../Data/", "../../Missions/", "../../Parsed Data/")
 
--- IOG/Parser.py
+++ IOG/Parser.py
@@ -18,6 +18,7 @@
 # Written by Jordan Jones and Nolan Heim
 #
 import os
+import numpy as np
 from Mission import *
 
 ###IMPORTANT - CHANGE THIS WHEN WE MOVE ON FROM PHASE 1###
@@ -52,33 +53,31 @@ class Parser:
         self.p_intervalStart = 4
         self.p_intervalEnd = 5
         
-        self.INDEX_OF_COORDINATES = 3
-
-   
-    def make_data_matrices(self):
-        data_matrices = []        
+        self.INDEX_OF_COORDINATES = 3   
         
-        for filename in os.listdir(self.datapath):
-            matrix = self.parse_data(filename)
-            data_matrices.append(matrix)
+   
+    def parse_data(self, parsed_datapath):
+        for filename in os.listdir(self.datapath): 
+            dataFile = os.path.join(self.datapath, filename)
+            fileObj = open(dataFile, 'r')
+            fileLines = [line.rstrip('\n') for line in fileObj]
+            fileLines[:] = [x for x in fileLines if x != '']
+            fileObj.close()
+        
+            dataStartIndex = fileLines.index('EphemerisTimePosVel') + 1
+            dataStopIndex = fileLines.index('END Ephemeris') - 1
+            dataMatrix = [[float(i) for i in fileLines[j].split()] for j in range(dataStartIndex,dataStopIndex)]
             
-        return data_matrices   
-        
-   
-    def parse_data(self, filename):
-        dataFile = os.path.join(self.datapath, filename)
-        fileObj = open(dataFile, 'r')
-        fileLines = [line.rstrip('\n') for line in fileObj]
-        fileLines[:] = [x for x in fileLines if x != '']
-        fileObj.close()
-        
-        dataStartIndex = fileLines.index('EphemerisTimePosVel') + 1
-        print(len(fileLines[dataStartIndex]))
-        dataStopIndex = fileLines.index('END Ephemeris') - 1
-        dataMatrix = [[float(i) for i in fileLines[j].split()] for j in range(dataStartIndex,dataStopIndex)]
-        
-        return dataMatrix
+            newFile = os.path.join(parsed_datapath, 'parsed_'+filename[0:-2])
+            
+            dataArray = []
+            extraInfo = [0, 0, 0, 0, 0, 0, 0]
+            dataArray.append(extraInfo)
+            dataArray.append(dataMatrix)
 
+            data = np.array(dataArray)
+            np.save(newFile, data)
+            
 
     def create_missions(self):
         missions = []        
@@ -155,5 +154,5 @@ class Parser:
 
 #Test code
 parser = Parser('../../Data/', '../../Missions/')
-parser.create_missions()
+parser.parse_data('../../Parsed Data/')
 
